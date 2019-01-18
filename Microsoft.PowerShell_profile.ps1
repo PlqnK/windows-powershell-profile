646562e5b50456986abb87582413348c48364400
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2019-01-18 17:00:47 +0100

    Remove shell window customisation

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,2 @@
 Set-Location C:\
-
-$Shell = $Host.UI.RawUI
-$Shell.CursorSize = 100
-$Shell.WindowSize.Width = 180
-$Shell.WindowSize.Height = 60
-$Shell.BufferSize.Width = 180
-$Shell.BufferSize.Height = 5000
-
 Clear-Host
