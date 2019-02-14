98f2eb8996661e8f6b1629e3ffee1b7bf4899da4
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2019-02-14 10:00:13 +0100

    Add Chocolatey profile loading

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,11 @@ Function Prompt {
     "$Path > "
 }
 
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
 Set-Location $Env:USERPROFILE
 Clear-Host
