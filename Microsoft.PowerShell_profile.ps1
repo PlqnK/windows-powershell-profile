c746859525b6785ec161f1bf6aa7b4ae92be0d54
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2020-01-09 09:44:46 +0100

    Fix various syntax errors, encoding and add .gitattributes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,13 +6,13 @@ Function Prompt {
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+    Import-Module "$ChocolateyProfile"
 }
 
 # Docker Machine environment variables
 $DockerMachine = "$env:ProgramFiles\Docker Toolbox\docker-machine.exe"
 if (Test-Path($DockerMachine)) {
-  & "$DockerMachine" env docker-host | Invoke-Expression
+    & "$DockerMachine" env docker-host | Invoke-Expression
 }
 
 Set-Location $env:USERPROFILE
